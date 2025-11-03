048c21d5c1eb8f6dae038c61afa45df2ca883949
Faisalkhan Pathan <faisalkhan.pathan.0100@gmail.com>
2025-11-03 13:17:05 +0530

    add different component like header,footer,sidebar,post,createpost,postlist and add dynamic chnage with conditional rendering for create post and view post

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
-
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import PostList from "./components/PostList";
+import CreatePost from "./components/CreatePost";
 
 function App() {
+  const [selectTab, setSelectTab] = useState("View Post");
+
+  const tabChange = (tab) => {
+    if (tab === "View Post") {
+      console.log(tab);
+      setSelectTab("View Post");
+    } else if (tab === "Create Post") {
+      console.log(tab);
+      setSelectTab("Create Post");
+    }
+  };
   return (
-    <>
-      <h1>Social Media</h1>
-    </>
+    <div className="app-container">
+      <Sidebar tabChange={selectTab} tabChange1={tabChange} />
+      <div className="content">
+        <Header />
+        {selectTab === "View Post" ? <PostList /> : <CreatePost />}
+        <Footer />
+      </div>
+    </div>
   );
 }
 
